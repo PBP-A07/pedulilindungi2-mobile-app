d518fa403ee069c45cc3ad281e3335550f970bf2
Ridjky Tegar Perkasa <ridjky.tegar@ui.ac.id>
2021-12-26 17:18:15 +0800

    Implement Sign Up and Sign In but without functionality and Radio Button

---
+++ account/lib/screens/sign_in.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+import '../widgets/primary_button.dart';
+import '../theme.dart';
+
+class SigninPage extends StatefulWidget {
+  const SigninPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SigninPageState createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+  bool passwordVisible = false;
+  bool passwordConfrimationVisible = false;
+  void togglePassword() {
+    setState(() {
+      passwordVisible = !passwordVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(24.0, 40.0, 24.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Halaman Sign In',
+                    style: heading2.copyWith(color: textBlack),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset(
+                    'assets/images/accent.png',
+                    width: 99,
+                    height: 4,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 48,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Email',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      child: TextFormField(
+                        obscureText: !passwordVisible,
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          suffixIcon: IconButton(
+                            color: textGrey,
+                            splashRadius: 1,
+                            icon: Icon(passwordVisible
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: togglePassword,
+                          ),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: textWhiteGrey,
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                      child: TextFormField(
+                        obscureText: !passwordConfrimationVisible,
+                        decoration: InputDecoration(
+                          hintText: 'Password Confirmation',
+                          hintStyle: heading6.copyWith(color: textGrey),
+                          suffixIcon: IconButton(
+                            color: textGrey,
+                            splashRadius: 1,
+                            icon: Icon(passwordConfrimationVisible
+                                ? Icons.visibility_outlined
+                                : Icons.visibility_off_outlined),
+                            onPressed: () {
+                              setState(() {
+                                passwordConfrimationVisible =
+                                    !passwordConfrimationVisible;
+                              });
+                            },
+                          ),
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+              CustomPrimaryButton(
+                buttonColor: primaryBlue,
+                textValue: 'Register',
+                textColor: Colors.white,
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Belum punya akun? ",
+                    style: regular16pt.copyWith(color: textGrey),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(
+                        context,
+                      );
+                    },
+                    child: Text(
+                      'Sign Up',
+                      style: regular16pt.copyWith(color: primaryBlue),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 32,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
