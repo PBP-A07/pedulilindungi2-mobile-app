c42bc6aa5815389914042ebd6116b7c9bcaed844
adnptrpw <andini.putri02@ui.ac.id>
2021-12-28 23:49:51 +0700

    change class name

--- daftar_vaksin/lib/daftar_vaksin.dart
+++ daftar_vaksin/lib/daftar_vaksin.dart
@@ -4,58 +4,39 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const DaftarVaksin());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class DaftarVaksin extends StatelessWidget {
+  const DaftarVaksin({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pedulilindungi2.0',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
         textTheme: GoogleFonts.rubikTextTheme(
           Theme.of(context).textTheme,
         ),
       ),
-      home: const MyHomePage(title: 'DAFTAR VAKSIN'),
+      home: const DaftarVaksinState(title: 'DAFTAR VAKSIN'),
     );
   }
 }
 
 
-class MyHomePage extends StatefulWidget {
+class DaftarVaksinState extends StatefulWidget {
 
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const DaftarVaksinState({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DaftarVaksinState> createState() => _DaftarVaksinState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DaftarVaksinState extends State<DaftarVaksinState> {
   String _kotaChoose = "Kota";
   String _tanggalChoose = "Tanggal";
   String _jenisVaksinChoose = "Jenis Vaksin";
@@ -71,16 +52,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
